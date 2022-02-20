1899984ef85234a1ac92629ffa33f61dfc79e165
Markos <markosmk@me.com>
2022-02-20 16:07:30 -0300

    update: added cors middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const routes = require('./routes')
+const routes = require('./routes');
 
-app.use('/v1', routes)
+app.use(cors());
+app.use('/v1', routes);
 
 app.listen(8080, () => {
-  console.log('servidor conectado');
+  console.log(`servidor conectado en http://localhost:8080`);
 });
