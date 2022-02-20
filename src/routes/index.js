22a9442926c31a742ed89906045ea152915f154a
Markos <markosmk@me.com>
2022-02-20 11:49:52 -0300

    update: added location middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const currentLocation = require('../middlewares/currentLocation');
+
 const controllerLocation = require('../controllers/location.controller');
 const controllerCurrent = require('../controllers/current.controller');
 const controllerForecast = require('../controllers/forecast.controller');
 
-router.get('/location', controllerLocation);
-router.get('/current/:city?', controllerCurrent);
-router.get('/forecast/:city?', controllerForecast);
+router.get('/location', currentLocation, controllerLocation);
+router.get('/current/:city?', currentLocation, controllerCurrent);
+router.get('/forecast/:city?', currentLocation, controllerForecast);
 
 module.exports = router;
