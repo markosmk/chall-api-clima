0fa36b8b22dc980d1cac533166a83b21c6caa511
Markos <markosmk@me.com>
2022-02-21 14:50:17 -0300

    update: added try catch to handle errors

--- src/controllers/current.controller.js
+++ src/controllers/current.controller.js
@@ -2,21 +2,25 @@ const fetch = require('node-fetch');
 const { API_URL_CURRENT_WEATHER } = require('../services/getApiData');
 
 const getCurrentWeather = async (req, res, next) => {
-  // 1) get location of current or city
-  const location = req.location;
-  // 2) get weather current
-  const responseWeather = await fetch(
-    `${API_URL_CURRENT_WEATHER}&lon=${location.lon}&lat=${location.lat}`
-  );
-  const current = await responseWeather.json();
+  try {
+    // 1) get location of current or city
+    const location = req.location;
+    // 2) get weather current
+    const responseWeather = await fetch(
+      `${API_URL_CURRENT_WEATHER}&lon=${location.lon}&lat=${location.lat}`
+    );
+    const current = await responseWeather.json();
 
-  res.json({
-    msg: 'success',
-    lat: location.lat,
-    lon: location.lon,
-    location,
-    current,
-  });
+    res.json({
+      msg: 'success',
+      lat: location.lat,
+      lon: location.lon,
+      location,
+      current,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = getCurrentWeather;
--- src/controllers/forecast.controller.js
+++ src/controllers/forecast.controller.js
@@ -2,19 +2,23 @@ const fetch = require('node-fetch');
 const { API_URL_ONECALL } = require('../services/getApiData');
 
 const getForecast = async (req, res, next) => {
-  // 1) get location of current or city
-  const location = req.location;
-  // 2) get weather for 5 days
-  const response = await fetch(
-    `${API_URL_ONECALL}&lat=${location.lat}&lon=${location.lon}`
-  );
-  const daily = await response.json();
+  try {
+    // 1) get location of current or city
+    const location = req.location;
+    // 2) get weather for 5 days
+    const response = await fetch(
+      `${API_URL_ONECALL}&lat=${location.lat}&lon=${location.lon}`
+    );
+    const daily = await response.json();
 
-  res.json({
-    msg: 'success',
-    location,
-    daily: daily.daily,
-  });
+    res.json({
+      msg: 'success',
+      location,
+      daily: daily.daily,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = getForecast;
