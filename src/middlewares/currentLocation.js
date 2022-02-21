65252eb24703d691c2712805a2c0d74dd157eb1c
Markos <markosmk@me.com>
2022-02-21 14:49:57 -0300

    update: added try catch to handle errors

--- src/middlewares/currentLocation.js
+++ src/middlewares/currentLocation.js
@@ -3,23 +3,27 @@ const { API_URL_GEOCODING } = require('../services/getApiData');
 
 const currentLocation = async (req, res, next) => {
   const city = req.params.city;
-  if (!city) {
-    const response = await fetch('http://ip-api.com/json/');
-    const location = await response.json();
-    if (!location) {
-      return res.json({ status: 404, msg: 'Ciudad No encontrada' });
-    }
-    req.location = location;
-  } else {
-    const response = await fetch(`${API_URL_GEOCODING}&q=${city}`);
-    const geolocation = await response.json();
+  try {
+    if (!city) {
+      const response = await fetch('http://ip-api.com/json/');
+      const location = await response.json();
+      if (!location) {
+        throw new Error('Ciudad No encontrada');
+      }
+      req.location = location;
+    } else {
+      const response = await fetch(`${API_URL_GEOCODING}&q=${city}`);
+      const geolocation = await response.json();
 
-    if (!geolocation || geolocation.length === 0) {
-      return res.json({ status: 404, msg: 'Ciudad No encontrada' });
+      if (!geolocation || geolocation.length === 0) {
+        throw new Error('Ciudad No encontrada');
+      }
+      req.location = geolocation[0]; // only first data of array
     }
-    req.location = geolocation[0]; // only first data of array
+    next();
+  } catch (error) {
+    next(error);
   }
-  next();
 };
 
 module.exports = currentLocation;
