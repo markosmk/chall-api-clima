e4e24ee9458ae1ad773141687547b1091a5989c7
Markos <markosmk@me.com>
2022-02-21 14:49:01 -0300

    update: add handleError

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const { invalidUrl, errorHandler } = require('./middlewares/handleErrors');
+
 const app = express();
 const routes = require('./routes');
 
 app.use(cors());
 app.use('/v1', routes);
+app.get('*', invalidUrl);
+app.use(errorHandler);
 
 app.listen(8080, () => {
   console.log(`servidor conectado en http://localhost:8080`);
